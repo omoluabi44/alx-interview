54c5d154b849f53ad8ccb6746a4a382c888c90c0
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-08 18:24:35 +0100

    task 1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,45 @@
-# 
+# #!/usr/bin/python3
+# """
+# lockboxed algorithm
+# """
+
+
+# def canUnlockAll(graph):
+#     """
+#     this sum everything in the list into set
+#     """
+#     int_list = []
+#     init_set = set()
+#     for i in graph:
+#         for j in i:
+#             int_list.append(j)
+#     init_set.update(int_list)
+#     init_set.add(0)
+
+#     """
+#     using graph to visit connected node
+#     """
+#     visited = [0]
+#     queue = [0]
+#     # visited.append(num)
+#     # queue.append(num)
+#     new_list = []
+#     new_set = set()
+
+#     while queue:
+#         m = queue.pop(0)
+#         new_list.append(m)
+#         for i in graph[m]:
+#             if i not in visited:
+#                 visited.append(i)
+#                 queue.append(i)
+#     new_set.update(new_list)
+#     if new_set == init_set:
+#         return True
+#     else:
+#         return False
 from collections import deque
+
 
 def canUnlockAll(graph):
     if not graph or len(graph) == 0:
