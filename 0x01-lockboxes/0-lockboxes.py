24d94a93eb03beeb09133e8f33aeec186151d36d
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-08 18:22:26 +0100

    litle bugs

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,40 +1,20 @@
-#!/usr/bin/python3
-"""
-lockboxed algorithm
-"""
-
+# 
+from collections import deque
 
 def canUnlockAll(graph):
-    """
-    this sum everything in the list into set
-    """
-    int_list = []
-    init_set = set()
-    for i in graph:
-        for j in i:
-            int_list.append(j)
-    init_set.update(int_list)
-    init_set.add(0)
+    if not graph or len(graph) == 0:
+        return True  # If there are no lockboxes, consider it as all unlocked
 
-    """
-    using graph to visit connected node
-    """
-    visited = [0]
-    queue = [0]
-    # visited.append(num)
-    # queue.append(num)
-    new_list = []
-    new_set = set()
+    n = len(graph)
+    visited = set([0])  # Start with room 0 as unlocked
+    queue = deque([0])  # Use deque for efficient pop from the left
 
     while queue:
-        m = queue.pop(0)
-        new_list.append(m)
-        for i in graph[m]:
-            if i not in visited:
-                visited.append(i)
-                queue.append(i)
-    new_set.update(new_list)
-    if new_set == init_set:
-        return True
-    else:
-        return False
+        current = queue.popleft()  # Get the next room to explore
+        for key in graph[current]:
+            if key not in visited and key < n:
+                visited.add(key)  # Mark the room as unlocked
+                queue.append(key)  # Explore the newly unlocked room
+
+    # If we visited all the rooms, return True
+    return len(visited) == n
